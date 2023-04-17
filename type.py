d13765d5d2c7bd4a87e6a18830d9e5741f83d23f
Danil <shashkoff.danil2016@yandex.ru>
2023-04-17 16:50:20 +0600

    try_5

--- type.py
+++ type.py
@@ -1,84 +1,70 @@
-import random
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+import data
 
 TOKEN_API = "6182928386:AAEJEAFlzUqK4iIkFzyDpTL5HKGMJ08zUy8"
 bot = Bot(TOKEN_API)
 dp =Dispatcher(bot)
 
-a={"Count": 0}
+#счётчик текущего вопроса и номер максильного вопроса
+counts={"Count": 0, "Count_max": 9}
 
-
-quest =["Вопрос №1: Какой сегодня день?","Вопрос №2: Кто автор «Сказки о попе и работнике его Балде»?","Вопрос №3: Что способствовало поражению ливонских рыцарей во время Ледового побоища?",
-       "Вопрос №4: Что находилось в сердцевине волшебной палочки Гарри Поттера?","Вопрос №5: Под каким названием известна единица с последующими ста нулями?"]
-answ =[["Сегодня","Вчера","Завтра", "Среда"], ["Гололь","Достоевский","Пушкин", "Крылов"], ["Началась пурга","Затупились коньки","Провалился лёд","Пропустили шайбу"],
-       ["Перо феникса","Волос единорога","Сердичная жила дракона","Цветок папоротника"],["Мегатрон","Гигадрон","Наномоль", "Гугол"]]
-
-
-zipped = list(zip(answ, quest))
-random.shuffle(zipped)
-answ_v2, quest_v2 = zip(*zipped)
-
-
+#что высвечивается при нажатии старта
 @dp.message_handler(commands=['start'], state=None)
-async def intro(message: types.Message): # асинхоронная функция
+async def begin(message: types.Message):
       markup = InlineKeyboardMarkup()
-      but_1 = InlineKeyboardButton("Играть", callback_data="but_1")
-      markup.add(but_1)
-      a["Count"]= 0
+      start = InlineKeyboardButton("Играть", callback_data="start")
+      markup.add(start)
+      counts["Count"]= 0
       await bot.send_message(message.chat.id, "Добро пожаловать в 'Как стать миллионером'", reply_markup=markup)
 
 
-
-
-
-@dp.callback_query_handler(lambda c: c.data == "but_1" or c.data == "Сегодня" or c.data == "Пушкин" or c.data == "Провалился лёд" or c.data == "Перо феникса" or c.data == "Гугол")
-async def first_quest(call: types.callback_query):
-    if (a["Count"]<= 4):
+#проверка на правильные ответы
+@dp.callback_query_handler(lambda c: c.data == "start" or c.data == "Сегодня" or c.data == "Пушкин" or c.data == "Провалился лёд" or c.data == "Перо феникса" or c.data == "Гугол" or
+                           c.data == "Билл Гейтс" or c.data == "Elk Cloner" or c.data == "Глубинное обучение" or c.data == "Айзек Азимов" or c.data == "12 сентября" or c.data == "HDD")
+async def game(call: types.callback_query):
+    if (counts["Count"]<= counts["Count_max"]): #вывод вопросов и ответов
         await  bot.answer_callback_query(call.id)
         markup = InlineKeyboardMarkup()
-        but_answ_1 = InlineKeyboardButton(answ_v2[a["Count"]][0], callback_data=answ_v2[a["Count"]][0])
-        but_answ_2= InlineKeyboardButton(answ_v2[a["Count"]][1], callback_data=answ_v2[a["Count"]][1])
-        but_answ_3= InlineKeyboardButton(answ_v2[a["Count"]][2], callback_data=answ_v2[a["Count"]][2])
-        but_answ_4 = InlineKeyboardButton(answ_v2[a["Count"]][3],  callback_data=answ_v2[a["Count"]][3])
+        but_answ_1 = InlineKeyboardButton(data.answ_v2[counts["Count"]][0], callback_data=data.answ_v2[counts["Count"]][0])
+        but_answ_2= InlineKeyboardButton(data.answ_v2[counts["Count"]][1], callback_data=data.answ_v2[counts["Count"]][1])
+        but_answ_3= InlineKeyboardButton(data.answ_v2[counts["Count"]][2], callback_data=data.answ_v2[counts["Count"]][2])
+        but_answ_4 = InlineKeyboardButton(data.answ_v2[counts["Count"]][3],  callback_data=data.answ_v2[counts["Count"]][3])
         markup.add(but_answ_1, but_answ_2)
         markup.add(but_answ_3, but_answ_4)
-        await bot.send_message(call.message.chat.id, quest_v2[a["Count"]], reply_markup=markup)
-        a["Count"] += 1
-    else:
+        await bot.send_message(call.message.chat.id, data.quest_v2[counts["Count"]], reply_markup=markup)
+        counts["Count"] += 1
+    else: #объявление победы и приглашение сыграть заново
         await bot.send_message(call.message.chat.id, "Подравляем, вы миллионер!")
         markup = InlineKeyboardMarkup()
-        but_return_yes = InlineKeyboardButton("Да", callback_data="but_1")
+        but_return_yes = InlineKeyboardButton("Да", callback_data="start")
         but_return_no = InlineKeyboardButton("Нет", callback_data="but_return_no")
         markup.add(but_return_yes, but_return_no)
         await bot.send_message(call.message.chat.id, "Хотите сыграть ещё раз?", reply_markup=markup)
-        a["Count"] = 0
+        counts["Count"] = 0
 
+
+#выход
 @dp.callback_query_handler(lambda c: c.data =="but_return_no")
 async def Exit(call: types.callback_query):
     await bot.answer_callback_query(call.id)
     await bot.send_message(call.message.chat.id, "Ну ладно...")
     exit(1)
 
-@dp.callback_query_handler(lambda c: c.data != "Сегодня" or c.data != "Пушкин" or c.data != "Провалился лёд" or c.data != "Перо феникса" or c.data != "Гугол")
+
+#обработка неправильных ответов и приглашение на повторную игру
+@dp.callback_query_handler()
 async def first_quest(call: types.callback_query):
     await  bot.answer_callback_query(call.id)
     await  bot.send_message(call.message.chat.id, "Вы проиграли!")
     markup = InlineKeyboardMarkup()
-    but_return_yes = InlineKeyboardButton("Да", callback_data="but_1")
+    but_return_yes = InlineKeyboardButton("Да", callback_data="start")
     but_return_no = InlineKeyboardButton("Нет", callback_data="but_return_no")
     markup.add(but_return_yes,but_return_no)
     await bot.send_message(call.message.chat.id, "Хотите сыграть ещё раз?", reply_markup=markup)
-    a["Count"] = 0
+    counts["Count"] = 0
 
-
-@dp.callback_query_handler(lambda c: c.data =="but_return_no")
-async def Exit(call: types.callback_query):
-    await  bot.answer_callback_query(call.id)
-    await  bot.send_message(call.message.chat.id, "Ну ладно...")
-    exit(1)
 
 def BOT():
    executor.start_polling(dp)
